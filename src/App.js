0c5255f680c699c85f80eb026a396ec11520c827
MikeHenze <yashuachrist@gmail.com>
2019-01-14 18:30:29 -0500

    creating facerecognition component and installing Clarifai client to use clarifai API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition'
 import './App.css';
 
+// particle.js library
 const particleOptions = {
   particles: {
     number: {
@@ -18,19 +21,51 @@ const particleOptions = {
   }
 }
 
+const app = new Clarifai.App({
+ apiKey: '7e7e51399a6e499681f1b1abc3389051'
+});
+
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+
+  app.models.predict(
+  Clarifai.FACE_DETECT_MODEL, 
+    this.state.input)
+  .then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+
+  }
+
   render() {
     return (
       <div className="App">
-      <Particles className='particles'
-        params={particleOptions} 
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*  
-        <FaceRecognition /> */}
+        <Particles className='particles'
+          params={particleOptions} 
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm  onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/> 
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
