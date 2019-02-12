59e22aef4d8a3052491e7aa12b9ddfea295f8fbe
MikeHenze <yashuachrist@gmail.com>
2019-02-12 12:07:00 -0500

    adding state to signin component and connecting frontend to backend

--- src/App.js
+++ src/App.js
@@ -41,6 +41,13 @@ class App extends Component {
     }
   }
 
+// connecting to backend server
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/')
+  //   .then(response => response.json())
+  //   .then(console.log);
+  // }
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
